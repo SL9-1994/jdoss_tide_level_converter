742eb2530d37838e3bb4611c61ba8faa05dde1b3
SL91994 <320sl91994@gmail.com>
2024-05-12 15:53:04 +0900

    hotfix: Fixed a bug that caused normal values to be converted as missing values.

--- src/converter.rs
+++ src/converter.rs
@@ -17,22 +17,47 @@ impl Converter {
     ///
     /// * The converted text data.
     pub fn convert_illegal_to_normal(&self, txt_data: String) -> String {
-        let mut chars = txt_data.chars().collect::<Vec<_>>();
-        let mut i = 0;
-        while i < chars.len() - 3 {
-            if chars[i] == '9' && chars[i + 1] == '9' && chars[i + 2] == '9' && chars[i + 3] == '9'
-            {
-                if i != 0 && chars[i - 1] != '\n' {
-                    chars.insert(i, ' ');
-                    i += 5; // 4つの9と新しく挿入したスペースをスキップ
-                } else {
-                    i += 4; // 4つの9をスキップ
-                }
-            } else {
-                i += 1;
+        let mut result = String::new();
+        for line in txt_data.lines() {
+            // 20個連続のスペースがあるかどうかをチェック
+            if line.chars().take(20).all(|c| c == ' ') {
+                continue; // スペースが20個連続している行はスキップ
             }
+
+            let mut chars = line.chars().collect::<Vec<_>>();
+            let mut i = 0;
+            while i < chars.len() - 3 {
+                if chars[i..i + 20].iter().all(|&c| c == ' ') {
+                    break; // 20個のスペースが見つかったら、その行の残りをスキップ
+                }
+                if chars[i] == '9'
+                    && chars[i + 1] == '9'
+                    && chars[i + 2] == '9'
+                    && chars[i + 3] == '9'
+                {
+                    if i + 4 >= chars.len() || chars[i + 4] != ' ' {
+                        chars.insert(i + 4, ' '); // 4つの9の後にスペースを挿入
+                    }
+                    i += 5; // 4つの9と新しく挿入したスペースをスキップ
+                } else if chars[i].is_digit(10)
+                    && chars[i + 1].is_digit(10)
+                    && chars[i + 2].is_digit(10)
+                    && (i + 3 >= chars.len() || chars[i + 3] != '-')
+                {
+                    if i + 3 >= chars.len() || chars[i + 3] != ' ' {
+                        chars.insert(i + 3, ' '); // 3つの数字の後にスペースを挿入
+                    }
+                    i += 4; // 3つの数字と新しく挿入したスペースをスキップ
+                } else {
+                    i += 1;
+                }
+            }
+
+            result.push_str(&chars.into_iter().collect::<String>());
+            result.push('\n');
         }
-        chars.into_iter().collect()
+
+        result
     }
 }
 
